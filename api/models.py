8defa22ab34fa391c2d24825d0717cea2ed9b759
Xednom <monde.lacanlalay@gmail.com>
2020-08-21 18:50:03 +0800

    added the patient extended information

--- api/models.py
+++ api/models.py
@@ -118,5 +118,101 @@ class PatientPhoneB02(BaseModel):
         return "%s %s" % (self.patient_contact, self.phone)
 
 
+class PatientSiblingB03(BaseModel):
+    RELATIONSHIP = (
+        ("Brother", "Brother"),
+        ("Sister", "Sister"),
+    )
+    patient_contact_b00_rec = models.ForeignKey(PatientContactB00, null=True, blank=True, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=500, null=True, blank=True)
+    middle_name = models.CharField(max_length=500, null=True, blank=True)
+    last_name = models.CharField(max_length=500, null=True, blank=True)
+    relationship = models.CharField(max_length=500, choices=RELATIONSHIP, null=True, blank=True)
+    age = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta: 
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return "%s %s - %s" % (self.last_name, self.first_name, self.relationship)
 
 
+class PatientGeneralQuestionsCategoryB04R(BaseModel):
+    name = models.CharField(max_length=500, null=True, blank=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return "%s %s" % (self.name, self.description)
+
+
+class PatientGeneralQuestionB04S(BaseModel):
+    patient_general_question_category_b04r_rec = models.ForeignKey(PatientGeneralQuestionsCategoryB04R, null=True, blank=True, on_delete=models.PROTECT)
+    question = models.TextField()
+    question_type = models.CharField(max_length=500, null=True, blank=True)
+    question_extended = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return "%s %s" % (self.patient_general_question_category_b04r_rec, self.question_type)
+
+
+class PatientGeneralQuestionB04(BaseModel):
+    patient_general_question_category_b04s_rec = models.ForeignKey(PatientGeneralQuestionB04S, null=True, blank=True, on_delete=models.PROTECT)
+    question = models.CharField(max_length=500, null=True, blank=True)
+    question_extended = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return "%s %s" % (self.patient_general_question_category_b04s_rec, self.question)
+
+
+class PatientFamilyB05S(BaseModel):
+    question = models.TextField()
+    mother = models.BooleanField(default=False)
+    mother_side = models.BooleanField(default=False)
+    father = models.BooleanField(default=False)
+    father_side = models.BooleanField(default=False)
+    brother = models.BooleanField(default=False)
+    sister = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id']
+    
+
+class PatientFamilyB05(BaseModel):
+    patient_contact_b00_rec = models.ForeignKey(PatientContactB00, null=True, blank=True, on_delete=models.PROTECT)
+    question = models.TextField()
+    mother = models.BooleanField(default=False)
+    mother_side = models.BooleanField(default=False)
+    father = models.BooleanField(default=False)
+    father_side = models.BooleanField(default=False)
+    brother = models.BooleanField(default=False)
+    sister = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class PatientGrowthB06(BaseModel):
+    patient_contact_b00_rec = models.ForeignKey(PatientContactB00, null=True, blank=True, on_delete=models.PROTECT)
+    question = models.TextField()
+    when = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class PatientHistoryB07(BaseModel):
+    patient_contact_b00_rec = models.ForeignKey(PatientContactB00, null=True, blank=True, on_delete=models.PROTECT)
+    question = models.TextField()
+    answer = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-id']
