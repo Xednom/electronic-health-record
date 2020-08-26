706f02534a975c718ddf40d1d484a154312b8b02
Xednom <monde.lacanlalay@gmail.com>
2020-08-26 20:03:02 +0800

    added the url for the entry point

--- ehr/urls.py
+++ ehr/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from .views import IndexTemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path('api/v1/', include("api.urls"))
+    path('api/v1/', include("api.urls")),
+
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point")
 ]
