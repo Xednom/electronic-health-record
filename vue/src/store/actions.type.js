8fb256135d5a71db79746d827f42cfa598aa78a8
Xednom <monde.lacanlalay@gmail.com>
2020-08-27 12:07:37 +0800

    removed 's' in actions for insurance rep

--- vue/src/store/actions.type.js
+++ vue/src/store/actions.type.js
@@ -4,6 +4,6 @@ export const FETCH_COMPANIES = "fetchCompanies";
 
 // insurance
 export const FETCH_AN_INSURANCE = "fetchAnInsurance";
-export const FETCH_AN_INSURANCE_REP = "fetchAnInsuranceReps";
+export const FETCH_AN_INSURANCE_REP = "fetchAnInsuranceRep";
 export const FETCH_INSURANCES = "fetchInsurances";
 export const FETCH_INSURANCE_REPS = "fetchInsuranceReps";
