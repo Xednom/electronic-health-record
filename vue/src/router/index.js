1fa315d85e9b6e72ad218ca4ba2635a49de01781
Xednom <monde.lacanlalay@gmail.com>
2020-08-27 20:08:44 +0800

    router for Company app

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+import CompanyAdd from "@/app/company/components/CompanyAdd.vue";
+import CompanyDetail from "@/app/company/components/CompanyEditor.vue";
+import CompanyList from "@/app/company/views/ListPage.vue";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,14 +15,30 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/company/add",
+    name: "company.add",
+    component: CompanyAdd
+  },
+  {
+    path: "/company/list",
+    name: "company.list",
+    component: CompanyList
+  },
+  {
+    path: "/company/:id/",
+    name: "company.edit",
+    component: CompanyDetail,
+    props: true
+  },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
