3fad31b8ef92344fcd495874ad67643d2a0c06d6
Xednom <monde.lacanlalay@gmail.com>
2020-08-27 20:14:11 +0800

    rename of components

---
+++ vue/src/ehr_components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand" :to="{ name: 'Home' }"
+      >Electronic Health Records</router-link
+    >
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ name: 'Home' }"
+            >Home</router-link
+          >
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Company
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <router-link class="dropdown-item" :to="{ name: 'company.list' }"
+              >List of Company</router-link
+            >
+            <router-link class="dropdown-item" :to="{ name: 'company.add' }"
+              >Add a company</router-link
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent"
+};
+</script>
+
+<style scoped>
+.nav-item {
+  margin: 0 20px 0 0;
+}
+.dropdown-menu {
+  right:0;
+  left:auto;
+}
+.my-navbar {
+  border-bottom: 1px solid #ddd;
+}
+.navbar-brand {
+  font-size: 100%;
+}
+</style>
