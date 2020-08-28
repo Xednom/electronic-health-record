1805eb088f8e0de229387d0ee8f917f9b6da5989
Xednom <monde.lacanlalay@gmail.com>
2020-08-28 14:20:25 +0800

    added insurance in navbar

--- vue/src/ehr_components/Navbar.vue
+++ vue/src/ehr_components/Navbar.vue
@@ -23,6 +23,19 @@
             >
           </div>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Insurance
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <router-link class="dropdown-item" :to="{ name: 'insurance.list' }"
+              >List of Insurance</router-link
+            >
+            <router-link class="dropdown-item" :to="{ name: 'insurance.add' }"
+              >Add an Insurance</router-link
+            >
+          </div>
+        </li>
       </ul>
     </div>
   </nav>
